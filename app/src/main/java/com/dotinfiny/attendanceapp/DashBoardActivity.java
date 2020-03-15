b2ac605ded048ab72b16359235ae7065e081c743
shehryarahmed <shehreyarahmed5@gmail.com>
2020-03-16 01:18:51 +0500

    update

--- app/src/main/java/com/dotinfiny/attendanceapp/DashBoardActivity.java
+++ app/src/main/java/com/dotinfiny/attendanceapp/DashBoardActivity.java
@@ -29,6 +29,7 @@ public class DashBoardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
